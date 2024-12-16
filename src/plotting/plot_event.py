01ceb92eb61dda3848fb02cb4fc77294b64f1114
Gregor Krzmanc <gregor.krzmanc@cern.ch>
2024-12-16 15:53:30 +0100

    sync notebooks

--- src/plotting/plot_event.py
+++ src/plotting/plot_event.py
@@ -4,18 +4,18 @@ def plot_event(event, ax=None):
     eta = event.pfcands.eta
     phi = event.pfcands.phi
     pt = event.pfcands.pt
-    mapping = event.pfcands.pf_cand_jet_idx
+    mapping = event.pfcands.pf_cand_jet_idx.int().tolist()
     print("N jets:", len(event.jets))
     genjet_eta = event.genjets.eta
     genjet_phi = event.genjets.phi
     genjet_pt = event.genjets.pt
-    if ax is not None:
+    if ax is None:
         fig, ax = plt.subplots(1, 2, figsize=(10, 5))
     # plot eta, phi and the size of circles proportional to p_t. The colors should be either gray (if not in mapping) or some other color that 'represents' the identified jet
     colorlist = ["red", "green", "blue", "purple", "orange", "yellow", "black", "pink", "cyan", "brown", "black", "black", "black", "gray"]
     colors = []
     for i in range(len(eta)):
-        colors.append(colorlist[mapping[i].item()])
+        colors.append(colorlist[mapping[i]])
     ax[0].scatter(eta, phi, s=pt, c=colors)
     ax[0].scatter(genjet_eta, genjet_phi, marker="*", s=genjet_pt, c="blue", alpha=1.0)
     # display the jets as a circ+le with R=0.5
@@ -28,25 +28,24 @@ def plot_event(event, ax=None):
     ax[0].set_ylabel(r"$\phi$")
     ax[0].set_title("PFCands with Jets")
     if event.fatjets is not None:
-        genjet_eta = event.genjets.eta
-        genjet_phi = event.genjets.phi
-        genjet_pt = event.genjets.pt
         colors = []
         for i in range(len(eta)):
-            colors.append(colorlist[mapping[i].item()])
+            colors.append(colorlist[mapping[i]])
         ax[1].scatter(eta, phi, s=pt, c=colors)
-        #ax[1].scatter(genjet_eta, genjet_phi, marker="*", s=genjet_pt, c="blue", alpha=1.0)
+        ax[1].scatter(genjet_eta, genjet_phi, marker="*", s=genjet_pt, c="blue", alpha=1.0)
         ax[1].set_xlabel(r"$\eta$")
         ax[1].set_ylabel(r"$\phi$")
         ax[1].set_title("PFCands with FatJets")
         # Plot the fatjets as a circle with R=0.8 around the center of the fatjet
         fatjet_eta = event.fatjets.eta
         fatjet_phi = event.fatjets.phi
-        #fatjet_pt = event.fatjets.pt
         fatjet_R = 0.8
         for i in range(len(fatjet_eta)):
             circle = plt.Circle((fatjet_eta[i], fatjet_phi[i]), fatjet_R, color="red", fill=False)
             ax[1].add_artist(circle)
+            # even aspect ratio
+            ax[1].set_aspect("equal")
+        ax[0].set_aspect("equal")
     if ax is not None:
         return fig
 
