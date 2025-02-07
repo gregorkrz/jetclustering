fix attention mask bug for virtual nodes

src/dataset/functions_data.py
@@ -521,6 +521,7 @@ class TensorCollection:
     #def __getitem__(self, i):
     #    return TensorCollection(**{k: v[i] for k, v in self.__dict__.items()})
 
+
 def get_corrected_batch(event_batch, cluster_idx):
     # return a batch with fake nodes in it (as .fake_nodes_idx property) and cluster_idx should be set to -1 for the nodes that don't belong anywhere
     # cluster_idx should be a tensor of the same length as the input vectors
@@ -539,22 +540,27 @@ def get_corrected_batch(event_batch, cluster_idx):
     #event_batch.input_scalars[clusters]
     #event_batch.pt[clusters]
     #
-    #return EventBatch(
-    #    input_vectors=torch.cat([event_batch.input_vectors[clusters], vectors_fake_nodes], dim=0),
-    #    input_scalars=torch.cat([event_batch.input_scalars[clusters], scalars_fake_nodes], dim=0),
-    #    pt=torch.cat([event_batch.pt[clusters], pt_fake_nodes], dim=0),
-    #    batch_idx=torch.cat([new_batch_idx, batch_idx_fake_nodes], dim=0),
-    #    fake_nodes_idx=batch_idx_fake_nodes + len(new_batch_idx),
-    # )
-    # For returning without the fake nodes (!!!!!)
-    print("New batch idx", renumber_clusters(new_batch_idx))
+    input_vectors = torch.cat([event_batch.input_vectors[clusters], vectors_fake_nodes], dim=0)
+    input_scalars = torch.cat([event_batch.input_scalars[clusters], scalars_fake_nodes], dim=0)
+    pt = torch.cat([event_batch.pt[clusters], pt_fake_nodes], dim=0)
+    batch_idx = torch.cat([new_batch_idx, batch_idx_fake_nodes], dim=0)
+    batch_sort_idx = torch.argsort(batch_idx) # the models need batch idx in ascending order in order to correctly construct the attention mask
     return EventBatch(
-        input_vectors=event_batch.input_vectors[clusters],
-        input_scalars=event_batch.input_scalars[clusters],
-        pt=event_batch.pt[clusters],
-        batch_idx=renumber_clusters(new_batch_idx)
-        #fake_nodes_idx=batch_idx_fake_nodes + len(new_batch_idx),
+        input_vectors=input_vectors[batch_sort_idx],
+        input_scalars=input_scalars[batch_sort_idx],
+        pt=pt[batch_sort_idx],
+        batch_idx=batch_idx[batch_sort_idx],
+        fake_nodes_idx=batch_idx_fake_nodes + len(new_batch_idx),
     )
+    #For returning without the fake nodes (!!!!!)
+    #print("New batch idx", renumber_clusters(new_batch_idx))
+    #return EventBatch(
+    #    input_vectors=event_batch.input_vectors[clusters],
+    #    input_scalars=event_batch.input_scalars[clusters],
+    #    pt=event_batch.pt[clusters],
+    #    batch_idx=renumber_clusters(new_batch_idx)
+    #    fake_nodes_idx=batch_idx_fake_nodes + len(new_batch_idx),
+    #)
 
 def get_batch(event, batch_config, y, test=False):
     # Returns the EventBatch class, with correct scalars etc.

src/models/transformer/transformer.py
@@ -2,7 +2,8 @@ from src.models.transformer.tr_blocks import Transformer
 import torch
 import torch.nn as nn
 from xformers.ops.fmha import BlockDiagonalMask
-from torch_scatter import scatter_max
+from torch_scatter import scatter_max, scatter_add, scatter_mean
+import numpy as np
 
 
 class TransformerModel(torch.nn.Module):
@@ -56,24 +57,38 @@ class TransformerModel(torch.nn.Module):
         print("input_dim", self.input_dim, inputs_transformer.shape)
         assert inputs_transformer.shape[1] == self.input_dim
         mask = self.build_attention_mask(data.batch_idx)
-        x = self.batch_norm(inputs_transformer).unsqueeze(0)
+        #x = self.batch_norm(inputs_transformer).unsqueeze(0)
+        #print("X before transformer pass", inputs_transformer[:10])
+        print("--- printing inputs into the transformer ---")
+        for batch in np.unique(data.batch_idx):
+            if batch < 5:
+                print("   -> batch ", batch, ":", inputs_transformer[data.batch_idx == batch][:5])
+
+        x = inputs_transformer.unsqueeze(0)
         x = self.transformer(x, attention_mask=mask)[0]
+        print("--- printing outputs from the transformer ---")
+        for batch in np.unique(data.batch_idx):
+            if batch < 5:
+                print("   -> batch ", batch, ":", x[data.batch_idx == batch][:5])
+        print("-----------------------------------------")
+        # maybe print the first 5 elements of each batch, before and after the transformer, to see why they become different at some point.
         #x = self.custom_decoder(x)
         assert x.shape[1] == self.output_dim, "Expected %d, got %d" % (self.output_dim, x.shape[1])
         assert x.shape[0] == inputs_transformer.shape[0], "Expected %d, got %d" % (inputs_transformer.shape[0], x.shape[0])
         if not self.obj_score:
             x[:, -1] = torch.sigmoid(x[:, -1])
         else:
-            extract_from_virtual_nodes = False
+            extract_from_virtual_nodes = True
             if extract_from_virtual_nodes:
                 x = self.final_mlp(x[data.fake_nodes_idx]) # x is the raw logits
             else:
-                print("x shape", x.shape)
-                print("data batch idx shape", data.batch_idx.shape)
-                m  = scatter_max(x, torch.tensor(data.batch_idx).to(x.device), dim=0)
-                print(m.shape)
-                x = self.final_mlp(m)
+                m  = scatter_mean(x, torch.tensor(data.batch_idx).long().to(x.device), dim=0)
+                print("m shape", m.shape)
+                print("x shape")
+                assert not "fake_nodes_idx" in data.__dict__
+                x = self.final_mlp(m).flatten()
         return x
+
     def build_attention_mask(self, batch_numbers):
         return BlockDiagonalMask.from_seqlens(
             torch.bincount(batch_numbers.long()).tolist()

src/utils/nn/tools_condensation.py
@@ -149,7 +149,7 @@ def train_epoch(
         del loss
         if (local_rank == 0) and (step_count % args.validation_steps) == 0:
             dirname = args.run_path
-            if obj_score_model is not None:
+            if obj_score_model is None:
                 model_state_dict = (
                     model.module.state_dict()
                     if isinstance(
@@ -177,7 +177,7 @@ def train_epoch(
                             torch.nn.parallel.DistributedDataParallel,
                         ),
                     )
-                    else model.state_dict()
+                    else obj_score_model.state_dict()
                 )
                 state_dict = {"model": model_state_dict, "optimizer": opt_obj_score.state_dict(),
                               "scheduler": sched_obj_score.state_dict()}